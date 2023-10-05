9253152173cafe1d808771056e35c332ed9f6926
Cgyulas0525 <cseszneki.gyula@gmail.com>
2023-10-05 16:42:56 +0200

    Fifth: roles table migration file, users table modify file

---
+++ database/migrations/2023_10_05_140739_add_users_table_more_columns.php
@@ -0,0 +1,37 @@
+<?php
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        if (Schema::hasColumn('users', 'phonenumber')) {
+            return;
+        }
+
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('phonenumber', 25)->nullable()
+                ->comment('format: +36-99-999-99-99');
+            $table->string('postcode', 6)->nullable();
+            $table->string('country', 50)->nullable();
+            $table->string('city', 50)->nullable();
+            $table->integer('role_id');
+            $table->string('address', 100)->nullable();
+            $table->string('description', 500)->nullable();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('phonenumber');
+            $table->dropColumn('postcode');
+            $table->dropColumn('country');
+            $table->dropColumn('city');
+            $table->dropColumn('role_id');
+            $table->dropColumn('address');
+            $table->dropColumn('description');
+        });
+    }
+};
